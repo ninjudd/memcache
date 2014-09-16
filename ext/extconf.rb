3b0dccfce69c5acac654d6922431121d4b96340d
Oleg Z <olegz@alertlogic.com>
2014-09-16 14:23:24 -0500

    Make it work with ruby 2.1

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -68,6 +68,7 @@ end
 # thanks to: https://gist.github.com/IanVaughan/5489431
 $CPPFLAGS += " -DRUBY_19" if RUBY_VERSION =~ /1.9/
 $CPPFLAGS += " -DRUBY_20" if RUBY_VERSION =~ /2.0/
+$CPPFLAGS += " -DRUBY_21" if RUBY_VERSION =~ /2.1/
 
 puts "*** Using Ruby version: #{RUBY_VERSION}"
 puts "*** with CPPFLAGS: #{$CPPFLAGS}"
